510b34c234df2806415c33c455a7bbfb1385f253
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-04-07 15:29:45 +0300

    Modifiying functions

--- src/slug/slug.service.ts
+++ src/slug/slug.service.ts
@@ -5,20 +5,24 @@ import { slugify } from 'slug-generator';
 @Injectable()
 export class SlugService {
   private slugs: Slug[] = [];
-  private currentId: number = 1;
+  private nextId: number = 1;
+
+  incrementNextId(): void {
+    this.nextId++;
+  }
 
   createNewSlug(original: string): Slug {
     const newSlugy = slugify(original);
-    if (this.slugs.find((item) => item.slug === newSlugy))
+    if (this.slugs.find((item) => item.slug === newSlugy)) {
       throw new BadRequestException('Slug already exists');
-    else {
+    } else {
       const newSlug: Slug = {
-        id: this.currentId,
+        id: this.nextId,
         original,
         slug: newSlugy,
       };
       this.slugs.push(newSlug);
-      this.currentId++;
+      this.incrementNextId();
       return newSlug;
     }
   }
@@ -27,19 +31,41 @@ export class SlugService {
     return this.slugs;
   }
 
+  lookForInvalidId(id: number): void {
+    if (id <= 0) {
+      throw new BadRequestException('Invalid id');
+    }
+  }
+
+  lookForValidId(id: number): void {
+    if (this.slugs.findIndex((item) => item.id === id) === -1) {
+      throw new BadRequestException(`Slug with id ${id} does not exist`);
+    }
+  }
+
   getById(id: number): Slug {
+    this.lookForInvalidId(id);
+    this.lookForValidId(id);
     const index = this.slugs.findIndex((item) => item.id === id);
-    if (index === -1) throw new BadRequestException('Slug does not exist');
-    else return this.slugs[index];
+    return this.slugs[index];
   }
 
   deleteById(id: number): string {
+    this.lookForInvalidId(id);
+    this.lookForValidId(id);
     const index = this.slugs.findIndex((item) => item.id === id);
-    if (index === -1) {
-      return `Slug with id ${id} does not exist`;
-    } else {
-      this.slugs.splice(index, 1);
-      return `Slug with id ${id} deleted successfully`;
-    }
+    this.slugs.splice(index, 1);
+    return `Slug with id ${id} deleted successfully`;
+  }
+
+  updateById(id: number, original: string): Slug {
+    this.lookForInvalidId(id);
+    this.lookForValidId(id);
+    const index = this.slugs.findIndex((item) => item.id === id);
+    const newSlug = this.slugs[index];
+    newSlug.original = original;
+    newSlug.slug = slugify(original);
+    this.slugs[index] = newSlug;
+    return newSlug;
   }
 }
