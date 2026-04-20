066df77026e4189e08a508cec9991e12ae6dec7d
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-04-20 19:23:17 +0300

    Add Prisma Access Layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SlugModule } from './slug/slug.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [SlugModule],
+  imports: [SlugModule, PrismaModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
